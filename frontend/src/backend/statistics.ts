6a183f0447852b3460c278fd7f9786659edb29af
phancongthanh <phancongthanhvtvpvn@gmail.com>
2023-04-17 19:46:51 +0700

    Bắt đầu statistics

---
+++ frontend/src/backend/statistics.ts
@@ -0,0 +1,12 @@
+import Statistics from '../types/Statistics';
+
+const statistics = {
+    get
+}
+
+export default statistics;
+
+export async function get() : Promise<Statistics> {
+    return {}
+
+}
